f26c65d4c81f91017be0b3f3211956bd7c462e9f
Sumner1185 <alastair.sumner@yahoo.com>
2020-09-25 14:00:46 +0100

    Green test for empty string

--- spec/ScrabbleSolver.test.js
+++ spec/ScrabbleSolver.test.js
@@ -3,108 +3,114 @@ ScrabbleSolver = require('../src/ScrabbleSolver.js')
 describe('ScrabbleSolver', () => {
 
   test('Should return 1 when given a as a parameter', () => {
-    scrabbleSolver = new ScrabbleSolver('a')
+    scrabble = new ScrabbleSolver('a')
 
-    expect(scrabbleSolver.score()).toEqual(1)
+    expect(scrabble.score()).toEqual(1)
   })
 
   test('Should return 2 when given d as a parameter', () => {
-    scrabbleSolver = new ScrabbleSolver('d')
+    scrabble = new ScrabbleSolver('d')
     
-    expect(scrabbleSolver.score()).toEqual(2)
+    expect(scrabble.score()).toEqual(2)
   })
 
   test('Should return 3 when given b as a parameter', () => {
-    scrabbleSolver = new ScrabbleSolver('b')
+    scrabble = new ScrabbleSolver('b')
     
-    expect(scrabbleSolver.score()).toEqual(3)
+    expect(scrabble.score()).toEqual(3)
   })
 
   test('Should return 4 when given f as a parameter', () => {
-    scrabbleSolver = new ScrabbleSolver('f')
+    scrabble = new ScrabbleSolver('f')
     
-    expect(scrabbleSolver.score()).toEqual(4)
+    expect(scrabble.score()).toEqual(4)
   })
 
   test('Should return 5 when given k as a parameter', () => {
-    scrabbleSolver = new ScrabbleSolver('k')
+    scrabble = new ScrabbleSolver('k')
     
-    expect(scrabbleSolver.score()).toEqual(5)
+    expect(scrabble.score()).toEqual(5)
   })
 
   test('Should return 8 when given j as a parameter', () => {
-    scrabbleSolver = new ScrabbleSolver('j')
+    scrabble = new ScrabbleSolver('j')
     
-    expect(scrabbleSolver.score()).toEqual(8)
+    expect(scrabble.score()).toEqual(8)
   })
 
   test('Should return 10 when given q as a parameter', () => {
-    scrabbleSolver = new ScrabbleSolver('q')
+    scrabble = new ScrabbleSolver('q')
     
-    expect(scrabbleSolver.score()).toEqual(10)
+    expect(scrabble.score()).toEqual(10)
   })
 
   test('Should return 2 when given aa as a parameter', () => {
-    scrabbleSolver = new ScrabbleSolver('aa')
+    scrabble = new ScrabbleSolver('aa')
     
-    expect(scrabbleSolver.score()).toEqual(2)
+    expect(scrabble.score()).toEqual(2)
   })
 
   test('Should return 1 when given any one point letters as a parameter', () => {
-    scrabbleSolverTest1 = new ScrabbleSolver('e')
-    scrabbleSolverTest2 = new ScrabbleSolver('i')
-    scrabbleSolverTest3 = new ScrabbleSolver('o')
+    scrabbleTest1 = new ScrabbleSolver('e')
+    scrabbleTest2 = new ScrabbleSolver('i')
+    scrabbleTest3 = new ScrabbleSolver('o')
 
-    expect(scrabbleSolverTest1.score()).toEqual(1)
-    expect(scrabbleSolverTest2.score()).toEqual(1)
-    expect(scrabbleSolverTest3.score()).toEqual(1)
+    expect(scrabbleTest1.score()).toEqual(1)
+    expect(scrabbleTest2.score()).toEqual(1)
+    expect(scrabbleTest3.score()).toEqual(1)
   })
 
   test('Should return 2 when given either two point letters as a parameter', () => {
-    scrabbleSolver = new ScrabbleSolver('g')
+    scrabble = new ScrabbleSolver('g')
 
-    expect(scrabbleSolver.score()).toEqual(2)
+    expect(scrabble.score()).toEqual(2)
   })
 
   test('Should return 3 when given all three point letters as a parameter', () => {
-    scrabbleSolverTest1 = new ScrabbleSolver('c')
-    scrabbleSolverTest2 = new ScrabbleSolver('m')
-    scrabbleSolverTest3 = new ScrabbleSolver('p')
+    scrabbleTest1 = new ScrabbleSolver('c')
+    scrabbleTest2 = new ScrabbleSolver('m')
+    scrabbleTest3 = new ScrabbleSolver('p')
 
-    expect(scrabbleSolverTest1.score()).toEqual(3)
-    expect(scrabbleSolverTest2.score()).toEqual(3)
-    expect(scrabbleSolverTest3.score()).toEqual(3)
+    expect(scrabbleTest1.score()).toEqual(3)
+    expect(scrabbleTest2.score()).toEqual(3)
+    expect(scrabbleTest3.score()).toEqual(3)
   })
 
   test('Should return 4 when given all four point letters as a parameter', () => {
-    scrabbleSolverTest1 = new ScrabbleSolver('h')
-    scrabbleSolverTest2 = new ScrabbleSolver('v')
-    scrabbleSolverTest3 = new ScrabbleSolver('w')
-    scrabbleSolverTest4 = new ScrabbleSolver('y')
+    scrabbleTest1 = new ScrabbleSolver('h')
+    scrabbleTest2 = new ScrabbleSolver('v')
+    scrabbleTest3 = new ScrabbleSolver('w')
+    scrabbleTest4 = new ScrabbleSolver('y')
 
-    expect(scrabbleSolverTest1.score()).toEqual(4)
-    expect(scrabbleSolverTest2.score()).toEqual(4)
-    expect(scrabbleSolverTest3.score()).toEqual(4)
-    expect(scrabbleSolverTest4.score()).toEqual(4)
+    expect(scrabbleTest1.score()).toEqual(4)
+    expect(scrabbleTest2.score()).toEqual(4)
+    expect(scrabbleTest3.score()).toEqual(4)
+    expect(scrabbleTest4.score()).toEqual(4)
   })
 
   test('Should return 8 when given either eight point letters as a parameter', () => {
-    scrabbleSolver = new ScrabbleSolver('x')
+    scrabble = new ScrabbleSolver('x')
 
-    expect(scrabbleSolver.score()).toEqual(8)
+    expect(scrabble.score()).toEqual(8)
   })
 
   test('Should return 10 when given either ten point letters as a parameter', () => {
-    scrabbleSolver = new ScrabbleSolver('z')
+    scrabble = new ScrabbleSolver('z')
 
-    expect(scrabbleSolver.score()).toEqual(10)
+    expect(scrabble.score()).toEqual(10)
   })
 
   test('Should return correct score for any lower case word', () => {
-    scrabbleSolverTest1 = new ScrabbleSolver('street')
-    scrabbleSolverTest2 = new ScrabbleSolver('quirky')
+    scrabbleTest1 = new ScrabbleSolver('street')
+    scrabbleTest2 = new ScrabbleSolver('quirky')
 
-    expect(scrabbleSolverTest1.score()).toEqual(6)
-    expect(scrabbleSolverTest2.score()).toEqual(22)
+    expect(scrabbleTest1.score()).toEqual(6)
+    expect(scrabbleTest2.score()).toEqual(22)
+  })
+
+  test('Should return 0 if given an empty string', () => {
+    scrabble = new ScrabbleSolver('')
+
+    expect(scrabble.score()).toEqual(0)
   })
 })
